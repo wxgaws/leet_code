ead0952c1e1fda6645a981251d50fc830f036f1c
xiaogang.wu <xiaogang.wu@zhaopin.com.cn>
2020-10-01 17:04:32 +0800

    S862.java: 和最少为K的最短子数组

--- src/main/java/leetcode/S862.java
+++ src/main/java/leetcode/S862.java
@@ -1,36 +1,34 @@
 package main.java.leetcode;
 
-import java.util.LinkedList;
+import java.util.*;
 
 class S862 {
-    public static int shortestSubarray(int[] A, int K) {
-        int N = A.length;
-        int[] p = new int[N + 1];
-        p[0] = 0;
-        for (int i = 1; i < N + 1; i++) {
-            p[i] = p[i - 1] + A[i - 1];
-        }
-
-        LinkedList<Integer> q = new LinkedList<>();
-        int res = N + 1;
-        for (int j = 0; j < N; j++) {
-            if ((q.isEmpty() && p[j + 1] > 0) || (!q.isEmpty() && p[j + 1] > p[q.getLast()])) {
-                q.addFirst(j);
+    class Solution {
+        public int shortestSubarray(int[] A, int K) {
+            int N = A.length;
+            int[] s = new int[N + 1];
+            s[0] = 0;
+            for (int i = 0; i < N; i++) {
+                s[i + 1] = s[i] + A[i];
             }
-            while (!q.isEmpty() && p[j + 1] - p[q.getLast()] >= K) {
-                System.out.println("before: Ai=" + q.getLast() + ",  Aj=" + j);
-                res = Math.min(res, j + 1 - q.removeLast());
-                System.out.println("after:    Ai=" + q.getLast() + ",  Aj=" + j + ", sum=" + (p[j + 1] - p[q.getLast()] - K));
+
+            Deque<Integer> que = new LinkedList();
+            int span = N + 1;
+            for (int j = 0; j < s.length; j++) {
+                //保证队单调
+                while (!que.isEmpty() && s[que.getLast()] >= s[j]) {
+                    que.removeLast();
+                }
+                //更新span
+                while (!que.isEmpty() && s[j] - s[que.getFirst()] >= K) {
+                    span = Math.min(span, j - que.removeFirst());
+                }
+                //提前退出
+                if (span == 1) return 1;
+                que.addLast(j);
             }
+            span = (span == N + 1) ? -1 : span;
+            return span;
         }
-
-        res = (res < N + 1) ? res : -1;
-        return res;
-    }
-
-    public static void main(String[] args) {
-        int[] A = {-34, 37, 51, 3, -12, -50, 51, 100, -47, 99, 34, 14, -13, 89, 31, -14, -44, 23, -38, 6};
-        int k = 151;
-        System.out.println(shortestSubarray(A, k));
     }
 }
