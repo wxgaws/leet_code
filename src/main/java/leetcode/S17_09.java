3031d33c6d25624b4330ce2f70da9edf3bc14d67
xiaogang.wu <xiaogang.wu@zhaopin.com.cn>
2020-10-01 16:28:27 +0800

    S17_09.java: 第k个丑数

---
+++ src/main/java/leetcode/S17_09.java
@@ -0,0 +1,19 @@
+package main.java.leetcode;
+
+public class S17_09 {
+    class Solution {
+        public int getKthMagicNumber(int k) {
+            int[] arr = new int[k];
+            int i3 = 0, i5 = 0, i7 = 0;
+            arr[0] = 1;
+            if (k == 1) return arr[0];
+            for (int i = 1; i < k; i++) {
+                arr[i] = Math.min(arr[i3] * 3, Math.min(arr[i5] * 5, arr[i7] * 7));
+                if (arr[i] % 3 == 0) i3++;
+                if (arr[i] % 5 == 0) i5++;
+                if (arr[i] % 7 == 0) i7++;
+            }
+            return arr[k - 1];
+        }
+    }
+}
