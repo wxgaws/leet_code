ca78d6b0c094fde4b391056a5e6c123d62ee8192
xiaogang.wu <xiaogang.wu@zhaopin.com.cn>
2020-10-01 11:34:09 +0800

    S346_1.java: 优化代码，使用三目运算，使用min函数

--- src/main/java/leetcode/S346_1.java
+++ src/main/java/leetcode/S346_1.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class S346_1 {
     class MovingAverage {
         //使用队列，队列的最大长为size
-        Deque<Integer> que = new LinkedList();
+        Deque<Integer> que = new LinkedList<>();
         double sum = 0;//当前求和
         int size = 0;//最大容量，超过时去掉队头元素
         int count = 0;//输入的数据个数
@@ -20,13 +20,9 @@ public class S346_1 {
         public double next(int val) {
             que.add(val);
             count++;
-            if (count > size) sum -= que.poll();
-            sum += val;
-            if (count > size) {
-                return sum / size;
-            } else {
-                return sum / count;
-            }
+            int head = count > size ? que.poll() : 0;
+            sum = sum - head + val;
+            return sum / Math.min(count, size);
         }
     }
 
