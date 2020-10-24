2fbdc3fa663f3f051902821c416a5581bc2bce89
xiaogang.wu <xiaogang.wu@zhaopin.com.cn>
2020-10-24 21:26:11 +0800

    LC84.java: 使用Arrays.copyOf(arrOld, lenNew)简化代码

--- src/main/java/leetcode/LC84.java
+++ src/main/java/leetcode/LC84.java
@@ -9,11 +9,7 @@ class LC84 {
     //用单调栈实现
     public int largestRectangleArea(int[] heights) {
         int maxArea = 0;
-        int[] heights2 = new int[heights.length + 1];
-        for (int i = 0; i < heights.length; i++) {
-            heights2[i] = heights[i];
-        }
-        heights2[heights.length] = 0;
+        int[] heights2 = Arrays.copyOf(heights, heights.length + 1);
         Stack<Integer> stack = new Stack<>();//index栈,对应元素递增a0<=a1<=a2
         for (int i = 0; i < heights2.length; i++) {
             while (!stack.isEmpty() && heights2[i] < heights2[stack.peek()]) {
