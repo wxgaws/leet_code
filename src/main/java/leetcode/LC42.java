3f38f7518ec8084e7119ea98cd0d0d3143a0710c
xiaogang.wu <xiaogang.wu@zhaopin.com.cn>
2020-10-24 15:08:23 +0800

    LC42.java: update

--- src/main/java/leetcode/LC42.java
+++ src/main/java/leetcode/LC42.java
@@ -11,19 +11,17 @@ class LC42 {
         int sum = 0;//当前雨水量
         Stack<Integer> stack = new Stack<>();//保存index
         for (int i = 0; i < height.length; i++) {
+            //比栈顶元素大，说明栈顶可以存水
             while (!stack.isEmpty() && height[i] > height[stack.peek()]) {
                 int top = stack.pop();
                 if (stack.isEmpty()) break;
                 int left = stack.peek();
                 int rain = (Math.min(height[i], height[left]) - height[top]) * (i - left - 1);
-                // System.out.printf("left=%d, top=%d, i=%d, rain=%d\n",left,top,i,rain);
                 sum += rain;
             }
 
+            //栈中已存元素时递减的
             stack.push(i);
-            // System.out.printf("i=%d, sum=%d, stack=%s\n",i,sum, stack.toString());
-
-
         }
 
         return sum;
