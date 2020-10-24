f7cac224bef761b56849665b33b4ac818bfbc967
xiaogang.wu <xiaogang.wu@zhaopin.com.cn>
2020-10-24 11:33:54 +0800

    Ms1.java: 数组的所有子串

--- src/main/java/leetcode/Ms1.java
+++ src/main/java/leetcode/Ms1.java
@@ -3,12 +3,16 @@ package main.java.leetcode;
 import java.util.*;
 
 public class Ms1 {
-
-    public Ms1() {
-    }
-
-    public static List<List<Integer>> subArr(int[] arr) {
-        List<List<Integer>> res = new ArrayList<>();
+    /**
+     * 问题：求数组arr[]的所有子串
+     * 思路：用递归的方式求解.
+     * 解法：
+     * 1，将数组arr的n个元素，拆违a0和剩余n-1个元素arrN1
+     * 2，arr的所有子串，包括三部分：a0, arrN1的所有子串，a1和arrN1的所有子串的交叉
+     * 3,对重复的子串要去重，用set，或者在加入list时判断是否存在。
+     */
+    public static Set<List<Integer>> subArr(int[] arr) {
+        Set<List<Integer>> res = new LinkedHashSet<>();
 
         if (arr.length == 1) {
             res.add(new ArrayList<>());
@@ -20,20 +24,16 @@ public class Ms1 {
         res.add(new ArrayList<>(Arrays.asList(arr[0])));
 
         //n-1
-        int[] arrN1 = new int[arr.length - 1];
-        for (int i = 0; i < arrN1.length; i++) {
-            arrN1[i] = arr[i + 1];
-        }
-        List<List<Integer>> resN1 = subArr(arrN1);
+        int[] arrN1 = Arrays.copyOfRange(arr, 1, arr.length);
+        Set<List<Integer>> resN1 = subArr(arrN1);
+        res.addAll(resN1);
+
+        //1,{n-1} cross
         for (List<Integer> list : resN1) {
-            if (!res.contains(list)) res.add(list);
-
-            //1,{n-1} cross
             List<Integer> list2 = new ArrayList<>();
-            list2.addAll(list);
             list2.add(arr[0]);
-            if (!res.contains(list2)) res.add(list2);
-
+            list2.addAll(list);
+            res.add(list2);
         }
 
         return res;
@@ -43,7 +43,7 @@ public class Ms1 {
         System.out.println("Ms1");
 
         int[] arr = {1, 2, 3};
-        List<List<Integer>> res = subArr(arr);
+        Set<List<Integer>> res = subArr(arr);
         for (List<Integer> list : res) {
             System.out.println(list);
         }
