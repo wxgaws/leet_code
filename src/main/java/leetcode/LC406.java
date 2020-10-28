e375800eea015ee9f15f7306ecec9d463b96bc5a
xiaogang.wu <xiaogang.wu@zhaopin.com.cn>
2020-10-28 17:09:12 +0800

    LC406.java: 根据身高重建队列

---
+++ src/main/java/leetcode/LC406.java
@@ -0,0 +1,27 @@
+package main.java.leetcode;
+
+import java.util.*;
+
+/**
+ * 根据身高重建队列
+ */
+class LC406 {
+    //排序和链表
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] a1, int[] a2) {
+                //0维降序，1维升序
+                int res = (a1[0] == a2[0]) ? (a1[1] - a2[1]) : (a2[0] - a1[0]);
+                return res;
+            }
+        });
+
+        LinkedList<int[]> list = new LinkedList<>();
+        for (int[] pair : people) {
+            list.add(pair[1], pair);
+        }
+
+        return list.toArray(new int[list.size()][2]);
+    }
+}
