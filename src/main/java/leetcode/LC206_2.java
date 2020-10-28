abfebacbf3d3bd91a9d3479b71ed6b71de3cc13c
xiaogang.wu <xiaogang.wu@zhaopin.com.cn>
2020-10-28 11:30:13 +0800

    LC206_2.java: 反转链表，循环方式

---
+++ src/main/java/leetcode/LC206_2.java
@@ -0,0 +1,30 @@
+package main.java.leetcode;
+
+import java.util.*;
+
+/**
+ * 反转链表
+ */
+class LC206_2 {
+    //循环
+    public ListNode reverseList(ListNode head) {
+        ListNode cur = head, pre = null;
+        while (cur != null) {
+            ListNode post = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = post;
+        }
+
+        return pre;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
