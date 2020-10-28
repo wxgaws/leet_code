afbbf50a8b9b05214debe258c868f7749aa060bd
xiaogang.wu <xiaogang.wu@zhaopin.com.cn>
2020-10-28 17:38:57 +0800

    ComparatorTest.java: 自定义比较器

---
+++ src/main/java/DataType/ComparatorTest.java
@@ -0,0 +1,59 @@
+package main.java.DataType;
+
+import java.util.*;
+
+public class ComparatorTest {
+
+    public static void testInterface() {
+        int[][] arr = {{7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}};
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                //0维降序，1维升序
+                int res = (o1[0] == o2[0]) ? (o1[1] - o2[1]) : (o2[0] - o1[0]);
+                return res;
+            }
+        });
+
+        System.out.println("testInterface");
+        for (int[] p : arr) System.out.println(Arrays.toString(p));
+
+    }
+
+    public static void testInterface2() {
+        int[][] arr = {{7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}};
+        Comparator<int[]> comparator = new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                //0维降序，1维升序
+                int res = (o1[0] == o2[0]) ? (o1[1] - o2[1]) : (o2[0] - o1[0]);
+                return res;
+            }
+        };
+        Arrays.sort(arr, comparator);
+
+        System.out.println("testInterface2");
+        for (int[] p : arr) System.out.println(Arrays.toString(p));
+
+    }
+
+    public static void testLambda() {
+        int[][] arr = {{7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}};
+        Arrays.sort(arr, (o1, o2) -> {
+            //0维降序，1维升序
+            int res = (o1[0] == o2[0]) ? (o1[1] - o2[1]) : (o2[0] - o1[0]);
+            return res;
+        });
+
+        System.out.println("testLambda");
+        for (int[] p : arr) System.out.println(Arrays.toString(p));
+    }
+
+    public static void main(String[] args) {
+        testInterface();
+        testInterface2();
+        testLambda();
+
+    }
+
+}
