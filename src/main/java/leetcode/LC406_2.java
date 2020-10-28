67657ec547f1abb834416a2110cb6cc1dc8bd9cf
wxgaws <wxgaws@163.com>
2020-10-28 21:35:26 +0800

    LC406_2.java: 用lambda表达式排序

---
+++ src/main/java/leetcode/LC406_2.java
@@ -0,0 +1,21 @@
+package main.java.leetcode;
+
+import java.util.*;
+
+class L406_2 {
+    public int[][] reconstructQueue(int[][] people) {
+        //用lambda表达式排序
+        Arrays.sort(people, (a1, a2) -> {
+            //0降序，1升序
+            int res = (a1[0] == a2[0]) ? (a1[1] - a2[1]) : (a2[0] - a1[0]);
+            return res;
+        });
+
+        List<int[]> list = new ArrayList<>();
+        for (int[] pair : people) {
+            list.add(pair[1], pair);
+        }
+
+        return list.toArray(new int[list.size()][2]);
+    }
+}
