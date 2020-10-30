a7962e127cb65daff65cb3f51873c2e35ec18a46
wxgaws <wxgaws@163.com>
2020-10-30 17:28:28 +0800

    CharTest.java: char是基本数据类型，没有equals()

---
+++ src/main/java/DataType/CharTest.java
@@ -0,0 +1,19 @@
+package main.java.DataType;
+
+import java.util.*;
+
+public class CharTest {
+
+    public static void test1() {
+        char c;
+        c = '(';
+        System.out.println(c == '(');
+        //char是基本数据类型，没有equals()
+//        System.out.println(c.equals('c'));
+    }
+
+    public static void main(String[] args) {
+        test1();
+    }
+
+}
