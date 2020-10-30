4984708f2f688f834e5946adc28095bb06483722
wxgaws <wxgaws@163.com>
2020-10-30 17:35:25 +0800

    LC678_2.java

---
+++ src/main/java/leetcode/LC678_2.java
@@ -0,0 +1,29 @@
+package main.java.leetcode;
+
+import java.util.*;
+
+/**
+ * 有效的括号字符串
+ */
+class LC678_2 {
+    //计算未匹配左括号的最大值和最小值
+    //使用char
+    public boolean checkValidString(String s) {
+        int min = 0, max = 0;//未匹配左括号的最大值和最小值
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                min++;
+                max++;
+            } else if (c == ')') {
+                if (min > 0) min--;
+                if (max-- == 0) return false;
+            } else {
+                if (min > 0) min--;
+                max++;
+            }
+        }
+
+        return min == 0;
+
+    }
+}
