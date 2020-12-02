5c2010d39a838b9975f43328009d26a73af3e6fc
wxgaws <wxgaws@163.com>
2020-12-02 13:44:38 +0800

    SetTest.java: update

--- src/main/java/DataType/SetTest.java
+++ src/main/java/DataType/SetTest.java
@@ -35,6 +35,19 @@ public class SetTest {
         System.out.println(set);
     }
 
+    @Test
+    public void test3() {
+        Set<Integer> set1 = new HashSet<>(Arrays.asList(1, 2, 3));
+        Set<Integer> set2 = new HashSet<>(Arrays.asList(4, 5, 6));
+        System.out.println("set1:" + set1 + ", " + set1.hashCode() + ", " + System.identityHashCode(set1));
+        System.out.println("set2:" + set2 + ", " + set2.hashCode() + ", " + System.identityHashCode(set2));
+        set2.clear();
+        set2.addAll(set1);
+        set1.add(4);
+        System.out.println("set1:" + set1 + ", " + System.identityHashCode(set1));
+        System.out.println("set2:" + set2 + ", " + System.identityHashCode(set2));
+    }
+
     public static void main(String[] args) {
 
 
