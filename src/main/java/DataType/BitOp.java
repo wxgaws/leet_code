a4b4d1a924c0a91582ed902cfc90883103356201
wxgaws <wxgaws@163.com>
2020-12-02 13:42:07 +0800

    BitOp.java: 位运算

---
+++ src/main/java/DataType/BitOp.java
@@ -0,0 +1,29 @@
+package main.java.DataType;
+
+import java.util.*;
+
+/**
+ * 位运算
+ */
+public class BitOp {
+
+    public BitOp() {
+    }
+
+    public static void swap(int a, int b) {
+        System.out.printf("a=%d, b=%d\n", a, b);
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        System.out.printf("a=%d, b=%d\n", a, b);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("BitOp");
+
+        swap(3, -107);
+
+    }
+
+}
