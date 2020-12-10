a0fe151b789a02e76d147976bc5e9813756f84cd
wxg <wxgaws@163.com>
2020-12-10 17:09:22 +0800

    test

---
+++ src/main/java/leetcode/test.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+import java.util.*;
+
+public class test {
+    public static void add(List<Integer> que, Integer val) {
+        System.out.println(que);
+        que.add(val);
+    }
+
+    public static void main(String args[]) {
+//        int[] arr = new int[10];
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.println(i + "," + arr[i]);
+//        }
+
+//        Set<String> list = new HashSet<>();
+//        list.addAll(Arrays.asList("1", "2", "4", "3"));
+//        list.add("5");
+////        Set<String> list = new HashSet<>(Arrays.asList("1", "2", "4", "3"));
+////        list.add("5");
+//        System.out.println(list);
+
+//
+//        int[][] arr = {{1, 2}, {4, 3}};
+//        for(int[] arr1:arr){
+//            System.out.println(Arrays.toString(arr1));
+//        }
+
+//        int a=5;
+//        System.out.println(a<<1);
+        for (int i = 6; i < 5; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
