7efdea9de9c3a23ec1e813025d2909662412e5fa
wxg <wxgaws@163.com>
2020-12-30 21:35:17 +0800

    StringTest.java: update

--- src/main/java/DataType/StringTest.java
+++ src/main/java/DataType/StringTest.java
@@ -1,5 +1,7 @@
 package DataType;
 
+import org.junit.Test;
+
 import java.util.*;
 
 public class StringTest {
@@ -15,6 +17,29 @@ public class StringTest {
         System.out.println("s=" + s);
         System.out.println(s == ")");
         System.out.println(s.equals(")"));
+    }
+
+    @Test
+    public void test2() {
+        Character c = 'A';
+        String s = c.toString();
+        System.out.println(c);
+        System.out.println(s);
+
+    }
+
+    @Test
+    public void test3() {
+        String s = "abcd";
+        System.out.println(s.substring(0,4));
+    }
+
+    @Test
+    public void test4() {
+        StringBuilder sb = new StringBuilder();
+        System.out.println(sb.toString().isEmpty());
+        System.out.println(sb.toString().equals(""));
+        System.out.println("".length());
     }
 
     public static void main(String[] args) {
