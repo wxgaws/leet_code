e4b616cf1dddaaeedd8b6cfacbe9600b1c01a34c
wxg <wxgaws@163.com>
2021-01-06 12:17:52 +0800

    CollectionsTest.java: update

--- src/main/java/DataType/CollectionsTest.java
+++ src/main/java/DataType/CollectionsTest.java
@@ -2,14 +2,21 @@ package DataType;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
 
 public class CollectionsTest {
     @Test
     public void test1() {
         Integer[] arr = {3, 7, -1, 4};
         System.out.println(Collections.max(Arrays.asList(arr)));
+    }
+
+    @Test
+    public void test2(){
+        Map<Integer,Integer>  map=new HashMap<>();
+        map.put(1,1);
+        map.put(2,2);
+        System.out.println(Collections.min(map.values()));
     }
 
 }
